948a3eafe78e6c5df3cdbab93e1398c4d056fbd2
Vivian <vivlopes95@gmail.com>
2024-09-17 15:16:23 +0100

    created new folders for customer facing logic and code

---
+++ ecommerce-site/src/app/(customerFacing)/layout.tsx
@@ -0,0 +1,19 @@
+import { Nav, NavLink } from "@/src/components/Nav";
+
+// This will force next.js to not cache any admin pages
+export const dynamic = "force-dynamic";
+
+export default function Layout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return <>
+        <Nav>
+            <NavLink href="/">Dashboard</NavLink>
+            <NavLink href="/products">Products</NavLink>
+            <NavLink href="/orders">My Orders</NavLink>
+        </Nav>
+        <div className="container my-6">{children}</div>
+    </>
+}
